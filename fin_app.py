a71416f344afdb921dbda1d9ee89517e3c622bdd
Ruslan Nizamov <nizamov.ruslan@gmail.com>
2022-02-09 12:50:26 -0800

    fix broken api calls, fix UI lists

--- fin_app.py
+++ fin_app.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
-import requests, json, csv
+import requests
+import json
+import csv
 from datetime import datetime, timezone
 import plotly.graph_objects as go
 import config
 
-'''
-application dashboard using finnhub api and streamlit
-'''
+
+# application dashboard using finnhub api and streamlit
+st.header('Welcome to Financial Dashboard')
 
 st.sidebar.title('Options')
 option = st.sidebar.selectbox(
-    'Which Dashboard?', ('Company News', 'Company Profile', 'News Sentiment', 'Insider Transactions', 'Chart'))
+    'Which Dashboard?', ('Company News', 'Company Profile', 'Insider Transactions'))
 st.title(option)
 
 if option == 'Insider Transactions':
@@ -20,12 +22,25 @@ if option == 'Insider Transactions':
         f"https://finnhub.io/api/v1/stock/insider-transactions?symbol={symbol}&token={config.token}")
     data = r.json()
     st.header(f"Insider selling for {symbol}")
+
     if len(data['data']) > 0:
-        for insider in data['data']:
-            st.write(insider['name'])
-            st.write(insider['share'])
-            st.write(insider['filingDate'])
-            st.write(insider['transactionPrice'])
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.header("Name")
+            for insider in data['data']:
+                st.write(insider['name'])
+        with col2:
+            st.header("Shares")
+            for insider in data['data']:
+                st.write(insider['share'])
+        with col3:
+            st.header("Date")
+            for insider in data['data']:
+                st.write(insider['filingDate'])
+        with col4:
+            st.header("Price")
+            for insider in data['data']:
+                st.write(f'$ {insider["transactionPrice"]}')
     else:
         st.subheader('No insider activity.')
 
@@ -54,63 +69,3 @@ if option == 'Company Profile':
     st.write("Country of company's headquarter: " + str(stock['country']))
     st.write("Market Capitalization: " + str(stock['marketCapitalization']))
     st.write("Number of oustanding shares: " + str(stock['shareOutstanding']))
-
-if option == 'News Sentiment':
-    symbol = st.sidebar.text_input('Symbol', value='AAPL', max_chars=5)
-    st.header(f"{symbol} News Sentiment")
-    r = requests.get(
-        f"https://finnhub.io/api/v1/news-sentiment?symbol={symbol}&token={config.token}")
-    stock = r.json()
-
-    st.write("Statistics of company news in the past week: " +
-             str(stock['buzz']['buzz']))
-    st.write("News score: " + str(stock['companyNewsScore']))
-    st.image(f"https://finviz.com/chart.ashx?t={symbol}")
-    st.write("Sector average bullish percent: " +
-             str(stock['sectorAverageBullishPercent']))
-    st.write("Sector average bearish percent: " +
-             str(stock['sentiment']['bearishPercent']))
-    st.write("Sector average bullish percent: " +
-             str(stock['sentiment']['bullishPercent']))
-
-if option == 'Chart':
-    symbol = st.sidebar.text_input('Symbol', value='AAPL', max_chars=5)
-    st.header(f"{symbol} Candlestick Chart")
-  
-    from_date = st.sidebar.slider("From what date",
-      value=datetime(2021, 1, 1),
-      format="MM/DD/YY")
-    
-    to_date = st.sidebar.slider("To what date",
-      value=datetime(2021, 1, 1),
-      format="MM/DD/YY")
-  
-    from_date_unix = int((from_date - datetime(1970, 1, 1)).total_seconds())
-    to_date_unix = int((to_date - datetime(1970, 1, 1)).total_seconds())
-    
-    st.write(from_date_unix)
-    st.write(to_date_unix)
-    
-    options = ['5', '15', '30', '60', 'D', 'W', 'M']
-    resolution = st.sidebar.radio('timeframe', options, index=3)
-        
-    r = requests.get(
-        f"https://finnhub.io/api/v1/stock/candle?symbol={symbol}&resolution={resolution}&from={from_date_unix}&to={to_date_unix}&token={config.token}")
-    data = r.json()
-    # st.write(data)
-
-    
-    df = pd.DataFrame(data)
-    # st.write(df)
-    fig = go.Figure(data=[go.Candlestick(x=pd.to_datetime(df['t'],unit='s'),
-                    open=df['o'],
-                    high=df['h'],
-                    low=df['l'],
-                    close=df['c'])])
-    fig.layout.xaxis.type = 'category'
-    fig.update_layout(height=600)
-    st.plotly_chart(fig, use_container_width=True)
-    st.write(df)
-    
-        
-    
